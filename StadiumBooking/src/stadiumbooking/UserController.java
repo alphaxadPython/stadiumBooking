b0a528c60a95c59faa020c1b11dbac20e8018a0e
alphaxadPython <alphaxadcoder@gmail.com>
2021-06-26 16:52:01 -0700

    saturday

--- StadiumBooking/src/stadiumbooking/UserController.java
+++ StadiumBooking/src/stadiumbooking/UserController.java
@@ -3,14 +3,76 @@ package stadiumbooking;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
 
 
 public class UserController implements Initializable {
+
+    @FXML
+    private Button home;
+    @FXML
+    private TabPane mainTab;
+    @FXML
+    private Button events;
+    @FXML
+    private Button venues;
+    @FXML
+    private Button stadium;
   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void goHome(MouseEvent event) {
+        mainTab.getSelectionModel().select(0);
+    }
+
+    @FXML
+    private void goEvents(MouseEvent event) {
+        mainTab.getSelectionModel().select(2);
+
+    }
+
+    @FXML
+    private void goVenues(MouseEvent event) {
+        mainTab.getSelectionModel().select(3);
+
+    }
+
+    @FXML
+    private void goStadium(MouseEvent event) {
+         mainTab.getSelectionModel().select(1);
+
+    }
+
+    @FXML
+    private void goBookStadium(MouseEvent event) {
+       mainTab.getSelectionModel().select(4);
+
+    }
+
+    @FXML
+    private void goBookEvent(MouseEvent event) {
+     mainTab.getSelectionModel().select(6);
+
+    }
+
+    @FXML
+    private void goBookVenue(MouseEvent event) {
+      mainTab.getSelectionModel().select(5);
+
+    }
+
+    @FXML
+    private void myBookings(MouseEvent event) {
+       mainTab.getSelectionModel().select(7);
+
+    }
     
 }
