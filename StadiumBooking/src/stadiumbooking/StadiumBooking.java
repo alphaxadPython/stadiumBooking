31bd5c7286681b8ff73ca64a2f0083452338b429
alphaxadPython <alphaxadcoder@gmail.com>
2021-06-26 13:38:38 -0700

    user

--- StadiumBooking/src/stadiumbooking/StadiumBooking.java
+++ StadiumBooking/src/stadiumbooking/StadiumBooking.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package stadiumbooking;
 
 import javafx.application.Application;
@@ -11,10 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author student
- */
+
 public class StadiumBooking extends Application {
     
     @Override
@@ -27,9 +20,7 @@ public class StadiumBooking extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         launch(args);
     }
