4f1eb27fe47f3215bd1d2e4cc70738ab7520edfa
alphaxadPython <alphaxadcoder@gmail.com>
2021-06-28 17:15:11 -0700

    forms

---
+++ StadiumBooking/src/stadiumbooking/LoginController.java
@@ -0,0 +1,19 @@
+
+package stadiumbooking;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class LoginController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- StadiumBooking/src/stadiumbooking/StadiumBooking.java
+++ StadiumBooking/src/stadiumbooking/StadiumBooking.java
@@ -12,12 +12,13 @@ public class StadiumBooking extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("AdminDashboard.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         
         Scene scene = new Scene(root);
         
         
         stage.setScene(scene);
+        
         stage.show();
     }
 
